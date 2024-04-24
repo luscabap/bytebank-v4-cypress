1056ac31caa214be27ba2b4c734045c9c8d077f5
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-04-24 15:50:56 -0300

    Teste jornada usuário ao realizar uma transação validando se funcionou corretamente no UI e na API

--- cypress/e2e/jornadaUsuario.cy.js
+++ cypress/e2e/jornadaUsuario.cy.js
@@ -1,21 +1,44 @@
 describe('Jornadas de usuário', () => {
+  const novaTransacao = {
+    tipoTransacao: 'Depósito',
+    valor: '105'
+  }
+
   it('Deve permitir que a pessoa usuária acesse a aplicação, realize uma transação e faça um logout', () => {
     cy.visit('/');
+    cy.fixture('usuarios').as('usuarios');
+    cy.get('@usuarios').then((usuario) => {
+      cy.getByDataTest('botao-login').click();
+      cy.getByDataTest('email-input').type(usuario[1].email);
+      cy.getByDataTest('senha-input').type(usuario[1].senha);
+      cy.getByDataTest('botao-enviar').click();
 
-    cy.getByDataTest('botao-login').click();
-    cy.getByDataTest('email-input').type('lucas@gmail.com');
-    cy.getByDataTest('senha-input').type('123');
-    cy.getByDataTest('botao-enviar').click();
+      cy.location('pathname').should('eq', '/home');
 
-    cy.location('pathname').should('eq', '/home');
+      cy.getByDataTest('select-opcoes').select(novaTransacao.tipoTransacao);
+      cy.getByDataTest('form-input').type(novaTransacao.valor);
+      cy.getByDataTest('realiza-transacao').click();
 
-    cy.getByDataTest('select-opcoes').select('Transferência');
-    cy.getByDataTest('form-input').type('80');
-    cy.getByDataTest('realiza-transacao').click();
+      cy.getByDataTest('lista-transacoes')
+        .find('li')
+        .last()
+        .contains(novaTransacao.valor);
 
-    cy.getByDataTest('lista-transacoes').find('li').last().contains('- R$ 80');
 
-    cy.getByDataTest('botao-sair').click();
-    cy.location('pathname').should('eq', '/');
+      cy.window().then(win => {
+        const userId = win.localStorage.getItem('userId');
+
+        cy.request({
+          method: 'GET',
+          url: `http://localhost:8000/users/${userId}/transations`,
+          failOnStatusCode: false
+        }).then(response => {
+          expect(response.status).to.equal(200);
+          expect(response.body).to.have.lengthOf.at.least(1);
+          expect(response.body[response.body.length - 1]).to.deep.include(novaTransacao)
+        })
+      }) 
+
+    });
   });
 });
