b47d545e9010740c6f961c92c0ac906e2d98fcf1
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-04-24 13:10:30 -0300

    Efetuando login e verificando nome, saldo e última transação feira

--- cypress/e2e/formularioLogin.cy.js
+++ cypress/e2e/formularioLogin.cy.js
@@ -8,21 +8,42 @@ describe('Formulario de Login', () => {
     cy.get('span').should('exist').and('have.text', 'E-mail ou senha incorretos')
   })
 
+  it('teste', () => {
+    cy.visit('/')
+  })
+
   it("Deve acessar a página home", () => {
     cy.efetuarLogin('lucas@gmail.com', '123')
     cy.visit('/home')
     cy.getByDataTest('titulo-boas-vindas').should('contain', 'Bem vindo de volta!');
   })
 
-  it.only("Deve acessar a página home", () => {
-    cy.fixture('usuarios').then(usuario => {
-      usuario.map(x => (
-        cy.efetuarLogin(x.email, x.senha), 
+  it("Deve acessar a página home", () => {
+    cy.fixture('usuarios').then(response => {
+      response.map(usuario => (
+        cy.efetuarLogin(usuario.email, usuario.senha), 
         cy.visit('/home'),
         cy.url().should('include', '/home'),
         cy.getByDataTest('titulo-boas-vindas').should('contain', 'Bem vindo de volta!'),
-        cy.contains(x.nome).should('be.visible')
+        cy.contains(usuario.nome).should('be.visible')
     ))
+    })
+  })
+
+  it.only("Deve exibir todas as informações do usuário corretamente", () => {
+    cy.fixture('usuariosCompletos.json').then(response => {
+      cy.efetuarLogin(response.email, response.senha);
+      cy.visit('/home')
+      cy.url().should('contain', '/home')
+      cy.getByDataTest('titulo-boas-vindas').should('exist')
+
+      cy.contains(response.nome).should('be.visible')
+
+      cy.getByDataTest('lista-transacoes').find('li').last()
+      .contains(response.transacoes[response.transacoes.length - 1].valor)
+
+      cy.get('[data-testid="saldo"]').contains(response.saldo)
+
     })
   })
 })
